fbf6c9e687f59df1c72ba8b8c805bf0c7f734ce7
tasksforwwork-svg <tasks.for.wwork@gmail.com>
2026-02-13 13:12:20 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,9 +1,11 @@
 from database.db import get_connection
 
+
 def create_tables():
     conn = get_connection()
     cursor = conn.cursor()
 
+    # Пользователи
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,14 +14,36 @@ def create_tables():
     )
     """)
 
+    # Категории
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        name TEXT,
+        parent_id INTEGER NULL
+    )
+    """)
+
+    # Транзакции
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS transactions (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         amount REAL,
         description TEXT,
+        category_id INTEGER,
         type TEXT,
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    # Бюджеты по категориям
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS budgets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        category_id INTEGER,
+        monthly_limit REAL
     )
     """)
 
