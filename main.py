36643276fb16fe8486a194653b06c4f48ac5076c
tasksforwwork-svg <tasks.for.wwork@gmail.com>
2026-02-17 12:05:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage # Важно для FSM
 from config import BOT_TOKEN
 
 from handlers import start
 from handlers import categories
 from handlers import transactions
+from handlers import reports # Добавили отчеты
 
 from database.models import create_tables
 
-
 async def main():
-    # Создаём таблицы при запуске
     create_tables()
 
     bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+    # Используем MemoryStorage для хранения состояний FSM
+    dp = Dispatcher(storage=MemoryStorage())
 
-    # ВАЖНО: порядок подключения роутеров
     dp.include_router(start.router)
     dp.include_router(categories.router)
     dp.include_router(transactions.router)
+    dp.include_router(reports.router) # Подключили роутер отчетов
 
-    # Запуск
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
