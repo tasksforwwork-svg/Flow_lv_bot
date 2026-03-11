21905b9aa5ca81418eed23df273ddce99bd2a864
tasksforwwork-svg <tasks.for.wwork@gmail.com>
2026-03-11 11:22:37 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -3,8 +3,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu_keyboard():
     keyboard = [
-        [KeyboardButton(text="➕ Добавить операцию")],
-        [KeyboardButton(text="📊 Отчёт за день")],
         [KeyboardButton(text="📅 Отчёт за месяц")],
         [KeyboardButton(text="💰 Остаток бюджета")],
         [KeyboardButton(text="⚙️ Категории")]
