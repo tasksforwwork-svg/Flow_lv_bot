2f1533f1fb3d1bdbda87fcdce04db2f083a3b4bd
tasksforwwork-svg <tasks.for.wwork@gmail.com>
2026-03-11 11:27:42 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -5,6 +5,7 @@ def main_menu_keyboard():
     keyboard = [
         [KeyboardButton(text="📅 Отчёт за месяц")],
         [KeyboardButton(text="💰 Остаток бюджета")],
+        [KeyboardButton(text="✏️ Редактировать")],
         [KeyboardButton(text="⚙️ Категории")]
     ]
 
